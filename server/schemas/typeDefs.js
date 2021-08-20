c49dc8b9b3c9a1120382c08e47c20bc4abb0ec77
ksfallon <fallon.katelin@gmail.com>
2021-08-20 11:13:34 -0400

    updated typeDefs.js to include the Auth type, and updated the User Type to unclude bookCount

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,41 +6,41 @@ type User {
     username: String
     email: String
     password: String
-    books: [Book]
+    bookCount: Int
+    savedbooks: [Book]
 }
 
 type Book {
     _id: ID!
+    bookId: String
     authors: [Author]
     description: String
-    bookId: String
+    title: String
     image: String
     link: String
-    title: String
 }
 
-# DO I NEED THIS TYPE HERE?!
-type Author {
-    _id: ID!
-    name: String
+type Auth {
+    token: ID!
+    user: User
 }
 
 type Query {
-    users: [User]
+    # users: [User]
     user(username: String!):User
-    books(username: String!): [Book]
-    book(bookId: ID!): Book
-    authors(bookId: String!): [Author]
-    author(authorId: String!): Author
+    # OR SHOULD IT BE
+    me(username: String!):User
+    # books(username: String!): [Book]
+    # book(bookId: ID!): Book
+    # authors(bookId: String!): [Author]
+    # author(authorId: String!): Author
 }
 
 type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(authors: String!, description: String!, bookId: String!, title: String!,): Book # should i also include the none required items here too?
-    # DO I NEED AN addAuthor?
+    saveBook(authors: String!, description: String!, bookId: String!, title: String!,): Book
     removeBook(bookId: ID!): Book
-    # DO I NEED A removeAuthor?
 }
 `
 module.exports = typeDefs;
