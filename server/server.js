1da82f494766dc83c05436922d90772c4603b6c0
ksfallon <fallon.katelin@gmail.com>
2021-08-20 08:42:03 -0400

    looked through backend utils/auth.js and not sure what to update there to get the middleware function to work with GraphQL API because it already appears to. So i created schema's folder and added the index.js file. All it includes is requiring typeDefs and resolvers and exports them

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ const { typeDefs, resolvers } = require('./schemas'); //need to require our sche
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({ // need the ApolloServer to call on the typeDefs and resolvers
+const server = new ApolloServer({ // need to set the ApolloServer to equal the typeDefs and resolvers
   typeDefs,
   resolvers,
 });
 
-server.applyMiddleware({ app }); // calling on the middleware here super important for our authorization
+server.applyMiddleware({ app }); // Now need our Server.Schemas to call on the middleware here super important for our authorization
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,7 +27,7 @@ app.get('*', (req, res) => { // here is our GET to grab our index.html
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
+db.once('open', () => { // once is an event listener on the database which is our user controller - creating all the users
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`)
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`) // need this so we can use graphql!!
