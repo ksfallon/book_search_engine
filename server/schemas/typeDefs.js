f34f0a24df395c0f1da3f8d12ac7e2bbf669479e
ksfallon <fallon.katelin@gmail.com>
2021-08-20 09:37:35 -0400

    added mutatinos to typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,45 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    books: [Book]
+}
 
+type Book {
+    _id: ID!
+    authors: [Author]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+# DO I NEED THIS TYPE HERE?!
+type Author {
+    _id: ID!
+    name: String!
+}
+
+type Query {
+    users: [User]
+    user(username: String!):User
+    books(username: String!): [Book]
+    book(bookId: ID!): Book
+    authors(bookId: String!): [Author]
+    author(authorId: String!): Author
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(authors: String!, description: String!, bookId: String!, title: String!,): Book # should i also include the none required items here too?
+    # DO I NEED AN addAuthor?
+    removeBook(bookId: ID!): Book
+    # DO I NEED A removeAuthor?
+}
+`
