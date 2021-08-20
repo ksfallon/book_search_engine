ca07e3afbd7e20e5885d31f3f60c8269d85db4c7
ksfallon <fallon.katelin@gmail.com>
2021-08-20 08:21:15 -0400

    Updated server to Add the ApolloServer, require our schemas, set that apollo server equal to the schemas, that new server to call on the middleware, do an app.get for our client/build/html path and finally include a console log of graphQL link in the app listener

--- README.md
+++ README.md
@@ -6,15 +6,17 @@ Link to [Budget Track Github page](https://github.com/ksfallon/book_search_engin
 
 ### **TABLE OF CONTENTS:**
 1. [Backend Changes](#1-backend-changes)
-2. [Server-js](#2-server-js)
+2. [Server-js and auth-js](#2-server-js-and-auth-js)
 3. [Schemas](#3-schemas)
 4. [Frontend Changes](#4-frontend-changes)
 5. [App-js](#5-app-js)
 
 # 1. Backend Changes
 
-## 2. Server-js
+## 2. Server-js and Auth-js
+Implement the Apollo Server and apply it to the Express server as middleware
 
+Update the auth middleware function to work with the GraphQL API.
 ## 3. Schemas
 
 # 4. Frontend Changes
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express'); // Need to require the apollo-server-express package to use graphQL
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas'); //need to require our schemas here for routing purposes
+// const routes = require('./routes'); No longer need to require routes because we are not doing a restful api anymore
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({ // need the ApolloServer to call on the typeDefs and resolvers
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app }); // calling on the middleware here super important for our authorization
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +22,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes); NO LONGER USING ROUTES FOR APIs -INSTEAD:
+app.get('*', (req, res) => { // here is our GET to grab our index.html
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`) // need this so we can use graphql!!
+  })
 });
