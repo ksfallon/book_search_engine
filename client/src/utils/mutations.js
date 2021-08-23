f013187a564f642010edcab3358574d64100cbf0
ksfallon <fallon.katelin@gmail.com>
2021-08-23 11:48:12 -0400

    added ADD_USER to mutations.js in client folder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,3 +13,17 @@ export const LOGIN_USER = gql`
     }
 `;
 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email  #don't know if i need email to be returned
+                password  #don't know if i need password to be returned
+            }
+        }
+    }
+`;
+
